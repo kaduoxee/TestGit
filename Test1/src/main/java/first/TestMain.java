ad111cf6a550b3080e8936f418c090b89bd39545
kaduoxee <2691850181@qq.com>
2022-03-17 18:34:40 +0800

    测试分支功能

--- Test1/src/main/java/first/TestMain.java
+++ Test1/src/main/java/first/TestMain.java
@@ -4,5 +4,7 @@ public class TestMain {
     public static void main(String[] args) {
         System.out.println("搞了老半天，终于可以用git了");
         System.out.println("可以放松一下了");
+        System.out.println("hello github");
+        System.out.println("我是test分支中的main方法");
     }
 }
---
+++ Test1/src/main/java/first/TestMain2.java
@@ -0,0 +1,7 @@
+package first;
+
+public class TestMain2 {
+    public static void main(String[] args) {
+        System.out.println("我存在于test分支中的TestMain2类中");
+    }
+}
