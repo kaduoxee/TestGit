9680d0f1f413450029377c9b700c57edc19a6f02
kaduoxee <2691850181@qq.com>
2022-03-17 18:12:27 +0800

    第二次更改

--- Test1/src/main/java/first/TestMain.java
+++ Test1/src/main/java/first/TestMain.java
@@ -4,5 +4,6 @@ public class TestMain {
     public static void main(String[] args) {
         System.out.println("搞了老半天，终于可以用git了");
         System.out.println("可以放松一下了");
+        System.out.println("hello git");
     }
 }
